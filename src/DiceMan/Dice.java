840bd28f978466919b64a76cf641a8c73d9f3a21
georg2990 <georg2990@gmail.com>
2025-09-05 15:30:52 +0200

    j

--- src/DiceMan/Dice.java
+++ src/DiceMan/Dice.java
@@ -3,9 +3,12 @@ package DiceMan;
 import java.util.Random;
 
 public class Dice {
-    private Random rand = new Random();
+    public int rollDice(){
+    Random rand = new Random();
 
-    int Dice = rand.nextInt(6) + 1;
-    Dice dice = new Dice();
+    int nr = rand.nextInt(6) + 1;
+    return nr;
+    }
+
 }
 
--- src/DiceMan/DiceMan.java
+++ src/DiceMan/DiceMan.java
@@ -1,4 +1,23 @@
 package DiceMan;
 
 public class DiceMan {
+    public String name;
+    public int numberOfActivity;
+
+    // konstruktør
+    public DiceMan(String name, int numberOfActivity) {
+        this.name = name;
+        this.numberOfActivity = numberOfActivity;
+    }
+    public void rollDice(){
+        int roll = 0;
+        Dice dice = new Dice();
+        roll = dice.rollDice();
+
+        if(roll == numberOfActivity){
+
+        }
+    }
+
+
 }
