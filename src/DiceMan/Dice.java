11b492238d3d04e202eca2d6438249599306d461
remo0002 <remo0002@stud.ek.dk>
2025-09-05 15:08:30 +0200

    .

--- src/DiceMan/Dice.java
+++ src/DiceMan/Dice.java
@@ -1,4 +1,11 @@
 package DiceMan;
 
+import java.util.Random;
+
 public class Dice {
+    private Random rand = new Random();
+
+    int Dice = rand.nextInt(6) + 1;
+    Dice dice = new Dice();
 }
+
